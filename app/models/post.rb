0365abc8a4f52b0d0f6b94eaa586b9d3bde65c6a
katherineabuhadal <katherine8@verizon.net>
2014-07-21 14:56:24 -0400

    Add validations to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :body, presence: true
+  validates :user_id, presence: true
+  validates :category_id, presence: true
 end
