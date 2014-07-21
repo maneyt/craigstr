3641555ca7ba732a7ab321c67327cdb1565e59de
Anthony Zotti <theillbehaviored@gmail.com>
2014-07-21 10:24:03 -0400

    Add admin priveledges to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140721134823) do
+ActiveRecord::Schema.define(version: 20140721141606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
+    t.string   "email",                            null: false
+    t.string   "password_digest",                  null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "privilege_type",  default: "user", null: false
   end
 
 end
