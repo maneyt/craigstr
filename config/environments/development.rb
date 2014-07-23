6784687bca64857f932be60bd65cdb5e8ac8fb4e
Thomas Maney <thomasjmaney@gmail.com>
2014-07-23 16:28:42 -0400

    Doesn't send email in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,7 +17,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
