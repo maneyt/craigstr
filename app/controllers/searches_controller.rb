f35e4088853dca22e5b98d7b17d3f4f403040ac8
katherineabuhadal <katherine8@verizon.net>
2014-07-24 16:44:27 -0400

    Refactor Search Controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,16 +1,23 @@
 class SearchesController < ApplicationController
   def show
-    @posts = Post.where(query_string, *search_terms)
+    @posts = searched_posts
   end
 
   private
 
-  def query_string
-    condition = "search_cache ILIKE ?"
-    search_terms.length.times.map { condition }.join(" AND ")
-  end
-
   def search_terms
     params[:q].split.map { |term| "%#{term}%"}
+  end
+
+  def searched_posts
+    posts = Post
+    search_terms.each do |term|
+      posts = posts.where(condition, term)
+    end
+    posts
+  end
+
+  def condition
+    "search_cache ILIKE ?"
   end
 end
