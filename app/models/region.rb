ef3a1992d96af7530b1b5bd0487aece9feae78d7
Thomas Maney <thomasjmaney@gmail.com>
2014-07-24 11:28:03 -0400

    Validate format region model

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,7 +1,7 @@
 class Region < ActiveRecord::Base
   has_many :categories
 
-  validates :name, uniqueness: true, presence: true
+  validates :name, uniqueness: true, presence: true, format: { with: /\A[a-zA-Z0-9 ]*\z/ }
 
   def to_param
     name
