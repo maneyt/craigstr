0ba18145d4a1d22af1b9577b802c273660660354
Anthony Zotti <theillbehaviored@gmail.com>
2014-07-24 11:40:52 -0400

    Fixed horizontol spacing

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,16 +2,12 @@ class Post < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :post_responses
-
   validates :title, presence: true
   validates :body, presence: true
   validates :user_id, presence: true
   validates :category_id, presence: true
   validates_length_of :title, minimum: 10, maximum: 60
   validates_length_of :body, minimum: 10, maximum: 750
-  
-
-
   paginates_per 10
   
   def mark_as_spam
