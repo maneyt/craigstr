f253672d7a0cab5e3f71b85df95a4e13ddaf7800
Thomas Maney <thomasjmaney@gmail.com>
2014-07-25 14:37:13 -0400

    Clean gmap.js

--- app/assets/javascripts/gmap.js
+++ app/assets/javascripts/gmap.js
@@ -1,11 +1,10 @@
 $(document).ready(function(){
-  var map;
   function initialize() {
     var mapOptions = {
       center: new google.maps.LatLng(40, 0),
       zoom: 2
     };
-    map = new google.maps.Map(document.getElementById("map-canvas"),
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
       mapOptions);
     for(var i = 0; i< regions.length; i++){
       var marker = new google.maps.Marker({
