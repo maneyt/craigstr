ade55bd0722fa0e5195bae36b8e3808b548c15ec
katherineabuhadal <katherine8@verizon.net>
2014-07-25 16:21:56 -0400

    Change font file

---
+++ config/initializers/assets.rb
@@ -0,0 +1,10 @@
+%w(bold black bolditalic italic regular).each do |style|
+  with_extensions = [
+    "fonts/lato/lato-#{style}.eot",
+    "fonts/lato/lato-#{style}.woff",
+    "fonts/lato/lato-#{style}.ttf",
+    "fonts/lato/lato-#{style}.svg",
+  ]
+
+  Rails.application.config.assets.precompile += with_extensions
+end
